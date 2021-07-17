eebfa5c5aced9925f68643b9d8d670d44e38e855
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-07-17 18:28:36 +0800

    Added role and user model eloquent relationships

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Socialite\Two\User;
 
 class Role extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
