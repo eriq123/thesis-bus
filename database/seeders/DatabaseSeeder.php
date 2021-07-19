4b4e053c957d925fbee327b7ad350dc9a51e4951
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-07-19 18:48:04 +0800

    Fix database seeder roles migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,25 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         DB::table('roles')->insert(
             [
-                'name' => 'Admin',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'name' => 'Bus Driver',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'name' => 'Bus Conductor',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'name' => 'Passenger',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
+
+                [
+                    'name' => 'Admin',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ],
+                [
+                    'name' => 'Bus Driver',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ],
+                [
+                    'name' => 'Bus Conductor',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ],
+                [
+                    'name' => 'Passenger',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ],
+            ]
         );
     }
 }
