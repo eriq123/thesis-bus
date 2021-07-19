9c117ea0076d17e3cdb0ef3b31d322acffadd59d
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-07-19 18:43:42 +0800

    Add proper roles to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Role::factory(10)->create();
+        DB::table('roles')->insert(
+            [
+                'name' => 'Admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Bus Driver',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Bus Conductor',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Passenger',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        );
     }
 }
