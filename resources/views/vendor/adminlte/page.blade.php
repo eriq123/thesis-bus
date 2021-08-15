b69d9beea932c3c5eb1b8dea0e4778c1f96a1ede
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-08-15 20:26:14 +0800

    Added error and success session alerts

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -3,14 +3,45 @@
 @inject('layoutHelper', 'JeroenNoten\LaravelAdminLte\Helpers\LayoutHelper')
 
 @if($layoutHelper->isLayoutTopnavEnabled())
-    @php( $def_container_class = 'container' )
+@php( $def_container_class = 'container' )
 @else
-    @php( $def_container_class = 'container-fluid' )
+@php( $def_container_class = 'container-fluid' )
 @endif
 
 @section('adminlte_css')
-    @stack('css')
-    @yield('css')
+@stack('css')
+@yield('css')
+<style>
+    .alert-danger {
+        color: #721c24;
+        background-color: #f8d7da;
+        border-color: #f5c6cb;
+    }
+
+    .alert-success {
+        color: #155724;
+        background-color: #d4edda;
+        border-color: #c3e6cb;
+    }
+
+    .fade-in {
+        opacity: 1;
+        animation-name: fadeInOpacity;
+        animation-iteration-count: 1;
+        animation-timing-function: linear;
+        animation-duration: 0.5s;
+    }
+
+    @keyframes fadeInOpacity {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
+</style>
 @stop
 
 @section('classes_body', $layoutHelper->makeBodyClasses())
@@ -18,55 +49,61 @@
 @section('body_data', $layoutHelper->makeBodyData())
 
 @section('body')
-    <div class="wrapper">
+<div class="wrapper">
 
-        {{-- Top Navbar --}}
-        @if($layoutHelper->isLayoutTopnavEnabled())
-            @include('adminlte::partials.navbar.navbar-layout-topnav')
-        @else
-            @include('adminlte::partials.navbar.navbar')
-        @endif
+    {{-- Top Navbar --}}
+    @if($layoutHelper->isLayoutTopnavEnabled())
+    @include('adminlte::partials.navbar.navbar-layout-topnav')
+    @else
+    @include('adminlte::partials.navbar.navbar')
+    @endif
 
-        {{-- Left Main Sidebar --}}
-        @if(!$layoutHelper->isLayoutTopnavEnabled())
-            @include('adminlte::partials.sidebar.left-sidebar')
-        @endif
+    {{-- Left Main Sidebar --}}
+    @if(!$layoutHelper->isLayoutTopnavEnabled())
+    @include('adminlte::partials.sidebar.left-sidebar')
+    @endif
 
-        {{-- Content Wrapper --}}
-        <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
-
-            {{-- Content Header --}}
-            @hasSection('content_header')
-                <div class="content-header">
-                    <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
-                        @yield('content_header')
-                    </div>
-                </div>
-            @endif
-
-            {{-- Main Content --}}
-            <div class="content">
-                <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
-                    @yield('content')
-                </div>
-            </div>
-
+    {{-- Content Wrapper --}}
+    <div class="content-wrapper {{ config('adminlte.classes_content_wrapper') ?? '' }}">
+        <div class="container-fluid pt-3">
+            @include('partials.alerts')
         </div>
 
-        {{-- Footer --}}
-        @hasSection('footer')
-            @include('adminlte::partials.footer.footer')
+        {{-- Content Header --}}
+        @hasSection('content_header')
+        <div class="content-header">
+            <div class="{{ config('adminlte.classes_content_header') ?: $def_container_class }}">
+                @yield('content_header')
+            </div>
+        </div>
         @endif
 
-        {{-- Right Control Sidebar --}}
-        @if(config('adminlte.right_sidebar'))
-            @include('adminlte::partials.sidebar.right-sidebar')
-        @endif
+        {{-- Main Content --}}
+        <div class="content">
+            <div class="{{ config('adminlte.classes_content') ?: $def_container_class }}">
+                @yield('content')
+            </div>
+        </div>
 
     </div>
+
+    {{-- Footer --}}
+    @hasSection('footer')
+    @include('adminlte::partials.footer.footer')
+    @endif
+
+    {{-- Right Control Sidebar --}}
+    @if(config('adminlte.right_sidebar'))
+    @include('adminlte::partials.sidebar.right-sidebar')
+    @endif
+
+</div>
 @stop
 
 @section('adminlte_js')
-    @stack('js')
-    @yield('js')
+@stack('js')
+@yield('js')
+<script>
+    $('.session-alert').delay(3000).fadeOut('slow');
+</script>
 @stop
