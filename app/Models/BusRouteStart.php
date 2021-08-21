f13779f4a785033686d1e3e801d5020b8e1431ff
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-08-22 00:42:29 +0800

    Added bus routes relations

--- app/Models/BusRouteStart.php
+++ app/Models/BusRouteStart.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class BusRouteStart extends Model
 {
     use HasFactory;
+
+    public function bus_routes()
+    {
+        return $this->hasMany(BusRoute::class);
+    }
 }
