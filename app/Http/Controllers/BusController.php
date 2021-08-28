61083ad6fab142e4e82ea9755a066dfd630bf9d9
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-08-29 04:00:30 +0800

    Remove unnecessary comments in bus controller

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -16,47 +16,23 @@ class BusController extends Controller
         $this->busRepository = $busRepository;
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('admin.bus.index', $this->busRepository->index());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->busRepository->store($request);
         return Redirect::route($this->DEFAULT_REDIRECT_ROUTE)->withSuccess('Added Successfully!');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request)
     {
         $this->busRepository->update($request);
         return Redirect::route($this->DEFAULT_REDIRECT_ROUTE)->withSuccess('Updated Successfully!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $this->busRepository->destroy($id);
