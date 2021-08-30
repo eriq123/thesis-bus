261c9ec643c64b5dd44db82037a035c690618741
Kisidi <kcalulo@gmail.com>
2021-08-30 23:57:48 +0800

    Import bus booking controller in api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BusRouteController;
+use App\Http\Controllers\Api\BusBookingController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
