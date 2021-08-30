0cc619b8e6d554f1ac290343af5a9cb5efa437b3
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-08-31 00:56:32 +0800

    Add booking api for passengers

--- app/Http/Controllers/Api/BusBookingController.php
+++ app/Http/Controllers/Api/BusBookingController.php
@@ -3,20 +3,54 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\BusBookingRepository;
+use App\Models\Booking;
+use App\Models\BusRoute;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BusBookingController extends Controller
 {
-    private $busBookingRepository;
-
-    public function __construct(BusBookingRepository $busBookingRepository)
+    public function stepOne()
     {
-        $this->busBookingRepository = $busBookingRepository;
+        $this->data['bus_routes'] = BusRoute::all();
+        return response()->json($this->busBookingRepository->index(), 200);
     }
 
-    public function index()
+    public function stepTwo(Request $request)
     {
-        return response()->json($this->busBookingRepository->index(), 200);
+        $this->validate($request, [
+            'starting_point_id' => 'required',
+            'destination_id' => 'required',
+            'schedule_date' => 'required',
+        ]);
+
+        $schedule = Schedule::where('starting_point_id', $request->starting_point_id)
+            ->where('destination_id', $request->destination_id)
+            ->where('schedule_date', $request->schedule_date)
+            ->with('bus')
+            ->get();
+
+        return response()->json($schedule, 200);
+    }
+
+    public function confirm(Request $request)
+    {
+        $this->validate($request, [
+            'schedule_id' => 'required',
+            'quantity' => 'required',
+        ]);
+
+        $schedule = Schedule::find($request->schedule_id);
+
+        $booking = new Booking();
+        $booking->user_id = Auth::user()->id;
+        $booking->schedule_id = $request->schedule_id;
+        $booking->fare_amount = $schedule->fare;
+        $booking->quantity = $request->quantity;
+        $booking->grand_total = $request->quantity * $schedule->fare;
+        $booking->status = 'Open';
+        $booking->save();
+
     }
 }
--- app/Repositories/BusBookingRepository.php
+++ app/Repositories/BusBookingRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repositories;
 
 use App\Models\Booking;
-use App\Models\Bus;
 use App\Models\BusRoute;
 use App\Models\Schedule;
 use App\Models\User;
@@ -11,11 +10,6 @@ use Illuminate\Support\Facades\Validator;
 
 class BusBookingRepository
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $this->data['schedules'] = Schedule::all();
@@ -24,71 +18,5 @@ class BusBookingRepository
         $this->data['passengers'] = User::where('role_id', 4)->get();
 
         return $this->data;
-    }
-
-    private function saveRequest($bus, $request)
-    {
-        $bus->plate_number = $request->plate_number;
-        $bus->type = $request->type;
-        $bus->capacity = $request->capacity;
-        $bus->save();
-    }
-
-    private function validateRequest($request, $isUpdate = false)
-    {
-        $rules = [
-            'plate_number' => 'required|max:255',
-            'type' => 'required|max:255',
-            'capacity' => 'required|numeric',
-        ];
-
-        $errorMessages = [
-            'plate_number.required' => 'Plate number field is required.',
-            'type.required' => 'Bus route field is required.',
-            'capacity.required' => 'Total seats field is required.',
-            'capacity.numeric' => 'Total seats field must be a number.'
-        ];
-
-        if ($isUpdate) $rules['id'] = 'required';
-
-        Validator::make($request->all(), $rules, $errorMessages)->validate();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store($request)
-    {
-        $this->validateRequest($request);
-        $bus = new Bus();
-        $this->saveRequest($bus, $request);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update($request)
-    {
-        $this->validateRequest($request, true);
-        $bus = Bus::find($request->id);
-        $this->saveRequest($bus, $request);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        Bus::destroy($id);
     }
 }
