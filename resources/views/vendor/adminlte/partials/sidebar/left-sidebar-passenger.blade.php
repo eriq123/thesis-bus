feb60733a0096c1b25419095d444252ae609ba08
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-09-05 18:14:18 +0800

    Update passenger left sidebar label

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar-passenger.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar-passenger.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="{{ route('buses.bookings.add') }}" class="nav-link">
         <i class="fas fa-book-open"></i>
-        <p class="ml-1">Book a Bus</p>
+        <p class="ml-1">Book a seat</p>
     </a>
 </li>
