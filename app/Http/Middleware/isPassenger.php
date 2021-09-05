13c0bc49e5fcb6e68ccb4a04c517d2a7c177d248
Eriq John Mendoza <mendozaeriqjohn@yahoo.com>
2021-09-05 17:29:30 +0800

    Update isAdmin and added isPassenger Middleware

--- app/Http/Middleware/isPassenger.php
+++ app/Http/Middleware/isPassenger.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class isAdmin
+class isPassenger
 {
     /**
      * Handle an incoming request.
@@ -17,7 +17,7 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if(Auth::user()->role_id !== 1) return redirect('/');
+        if(Auth::user()->role_id !== 4) return redirect('/');
 
         return $next($request);
     }
