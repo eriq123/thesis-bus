551fb835da8367beac3a75fe9628eaabe08d7706
basim <basim.usman94@gmail.com>
2021-11-09 06:02:47 +0500

    Added API for end Travel with updated status table entry of Finish

--- app/Http/Controllers/Api/BusBookingController.php
+++ app/Http/Controllers/Api/BusBookingController.php
@@ -11,6 +11,7 @@ use App\Models\User;
 use App\Repositories\BusBookingRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class BusBookingController extends Controller
 {
@@ -130,11 +131,71 @@ class BusBookingController extends Controller
             $result["remarks"] = "Verfied onBoard Successfully";
 
 
+        }elseif($health == 'end'){
+
+            $general_id ="";
+            $results = DB::select( DB::raw("SELECT * FROM bookings WHERE conductor_id = '$userId' AND (status_id = 6 OR status_id = 1 OR status_id = 2 OR status_id = 3)  "));
+           
+            for ($i = 0, $c = count($results); $i < $c; ++$i)
+            {
+                $results[$i] = (array) $results[$i];
+            }
+
+            for ($j = 0, $d = count($results); $j < $d; ++$j)
+            {
+                $schedule_id            = $results[$j]['schedule_id'];
+
+                $schedules              = Schedule::find($schedule_id);
+
+                if($schedules->status=="open"){
+
+                    $driver_id              = $results[$j]['driver_id'];
+                    $bus_id                 = $results[$j]['bus_id'];
+                    $passenger_id           = $results[$j]['user_id'];
+                    $booking_id             = $results[$j]['id'];
+                    $schedule_id_open       = $results[$j]['schedule_id'];
+
+
+                    $conductorResult   = User::find($userId);
+                    $conductorResult->status = 'open';
+                    $conductorResult->save();
+
+                    $driverResult      = User::find($driver_id);
+                    $driverResult->status = 'open';
+                    $driverResult->save();
+
+                    $busResult      = Bus::find($bus_id);
+                    $busResult->status = 'free';
+                 
+                    $busResult->save();
+
+                    $passengerResult         = User::find($passenger_id);
+                    $passengerResult->status = 'open';
+                    $passengerResult->save();
+
+                    $bookingResult            = Booking::find($booking_id);
+                    $bookingResult->status_id = 7;
+                    $bookingResult->save();
+                      
+                    $general_id = $schedule_id_open;
+
+                }else{
+                    continue;
+                }
+
+
+              
+            }
+
+             $schedules         = Schedule::find($general_id);
+             $schedules->status = "done";
+             $schedules->save();
+           
+            $result["status"] = TRUE;
+            $result["remarks"] = "Travel is Finished Successfully";
+
+
         }
-
-      
-
-
          return response()->json($result, 200);
     }
 }
